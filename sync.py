44c9b70ed9535fd1740847771980b29114227046
zml2008 <zach@zachsthings.com>
2013-05-15 20:18:41 -0700

    Remove some debug output and clean up file ignoring

--- sync.py
+++ sync.py
@@ -5,6 +5,14 @@ import os
 import shutil
 from os.path import *
 
+IGNORE_FILES = [
+        basename(__file__),
+        "README"
+        ]
+
+IGNORE_DIRS = [
+        ".git"
+        ]
 
 def check_dir(path):
     dir_name = dirname(path)
@@ -17,22 +25,25 @@ def main():
     root_dir = dirname(__file__)
     home = os.environ["HOME"]
     for root, dirs, files in os.walk(root_dir):
-        if root == root_dir: files.remove(basename(__file__))
-        if ".git" in dirs: dirs.remove(".git")
+        for i in IGNORE_DIRS:
+            if i in dirs: dirs.remove(i)
 
         for f in files:
             tip = join(root, f)
             rel_path = tip[(len(root_dir) + 1):]
+            if rel_path in IGNORE_FILES:
+                continue
+
             home_path = abspath(join(home, rel_path))
             check_dir(home_path)
-            print(home_path, "| File:", isfile(home_path), "Link:", isdir(home_path))
+
             if isfile(home_path) and not islink(home_path):
                 bak_path = home_path 
                 while isfile(bak_path):
                     bak_path = bak_path + ".bak"
-
                 shutil.move(home_path, bak_path)
-            os.remove(home_path)
+
+            os.remove(home_path) # Remove the existing symlink
             os.symlink(abspath(tip), home_path)
 
 
