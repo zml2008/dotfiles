d1847bc1d6b536f21ecc927b8a75a9e11411c52e
zml2008 <zach@zachsthings.com>
2013-08-10 10:54:23 -0700

    Work on the mediakeys handling

--- .config/awesome/mediakeys.lua
+++ .config/awesome/mediakeys.lua
@@ -4,8 +4,14 @@ local pa = require("pulseaudio.pulseaudio")
 
 
 keybindings.globalkeys(awful.util.table.join(
-	awful.key({}, "XF86MonBrightnessDown", function () awful.util.spawn("xbacklight -dec 5", false) end),
-	awful.key({}, "XF86MonBrightnessUp", function () awful.util.spawn("xbacklight -inc 5", false) end),
+	awful.key({}, "XF86MonBrightnessDown", function () coroutine.resume(coroutine.create(function() 
+			awful.util.spawn("xbacklight -dec 5", false) 
+		end))
+	end),
+	awful.key({}, "XF86MonBrightnessUp", function () coroutine.resume(coroutine.create(function() 
+			awful.util.spawn("xbacklight -inc 5", false) 
+		end)) 
+	end),
 	awful.key({}, "XF86AudioMute", function () pulseaudio.volumeMute() end),
 	awful.key({}, "XF86AudioLowerVolume", function () pulseaudio.volumeDown() end),
 	awful.key({}, "XF86AudioRaiseVolume", function () pulseaudio.volumeUp() end),
