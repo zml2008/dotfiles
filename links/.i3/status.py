f6ef330db041085f030c2dc268e4f94be512cad1
zml <zml@aoeu.xyz>
2016-06-26 19:37:57 -0700

    i3 config: Add caffeine-style bar widget

--- links/.i3/status.py
+++ links/.i3/status.py
@@ -3,8 +3,52 @@
 import subprocess
 import os
 
-from i3pystatus import Status
+from i3pystatus import Status, IntervalModule
 from i3pystatus.mail import maildir
+
+
+class NoLockIndicator(IntervalModule):
+    interval = 2
+
+    settings = (
+            ('path', 'The path of the file to toggle for nolock status. By default ~/.nolock'),
+            ('status', 'Dictionary mapping locked and unlocked to messages'),
+            ('status_color', 'Dictionary mapping locked and unlocked to colors')
+            )
+
+    path = '~/.nolock'
+    status = {
+            'locked': '☕',
+            'unlocked': 'O'
+            }
+    status_color = {
+            'locked': '#ff0000'
+            }
+
+    on_leftclick = 'toggle_state'
+
+    def run(self):
+        pathname = os.path.abspath(os.path.expanduser(self.path))
+        if os.path.exists(pathname):
+            state = "locked"
+        else:
+            state = "unlocked"
+
+        self.output = {
+                'full_text': self.status[state]
+                }
+
+        if state in self.status_color:
+            self.output['color'] = self.status_color[state]
+
+    def toggle_state(self):
+        pathname = os.path.abspath(os.path.expanduser(self.path))
+        if os.path.exists(pathname):
+            os.unlink(pathname)
+        else:
+            with open(pathname, 'w'):
+                pass
+
 
 status = Status(standalone=True)
 
@@ -21,5 +65,7 @@ for acc in os.listdir(mail_root):
 
 status.register("now_playing", format="{status} {title} - {artist}")
 status.register("pulseaudio", format="{muted}: {db}dB", unmuted="♪")
+
+status.register(NoLockIndicator())
 
 status.run()
